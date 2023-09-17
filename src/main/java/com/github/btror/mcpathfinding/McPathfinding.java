20c1f6aa23d06c4ffc044f0cc19f02265b2d00af
Brandon <ticer1999@gmail.com>
2023-09-16 20:45:13 -0400

    chore: remove commands

--- src/main/java/com/github/btror/mcpathfinding/McPathfinding.java
+++ src/main/java/com/github/btror/mcpathfinding/McPathfinding.java
@@ -1,23 +1,17 @@
 package com.github.btror.mcpathfinding;
 
 import com.github.btror.mcpathfinding.animation.Animation;
-import com.github.btror.mcpathfinding.commands.McPathfindingCommand;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
 
 public final class McPathfinding extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("mc-pathfinding enabled...");
-
-        McPathfindingCommand command = new McPathfindingCommand(this);
-        Objects.requireNonNull(getCommand("mcpathfinding")).setExecutor(command);
-        getServer().getPluginManager().registerEvents(command, this);
     }
 
     @Override
